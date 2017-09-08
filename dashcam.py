338b33110f3113e338caf9808dd3c4363472d071
the-raspberry-pi-guy <theraspberrypiguy@gmail.com>
2017-09-08 15:39:40 +0000

    Add modifications

--- dashcam.py
+++ dashcam.py
@@ -4,3 +4,13 @@
 
 import pygame
 import picamera
+import os
+
+os.putenv('SDL_VIDEODRIVER', 'fbcon')
+os.putenv('SDL_FBDEV'      , '/dev/fb1')
+os.putenv('SDL_MOUSEDRV'   , 'TSLIB')
+os.putenv('SDL_MOUSEDEV'   , '/dev/input/touchscreen')
+
+pygame.init()
+pygame.mouse.set_visible(False)
+screen = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
