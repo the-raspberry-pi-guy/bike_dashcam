4e71a65ea595fadba141eeb45269053bb0806881
the-raspberry-pi-guy <theraspberrypiguy@gmail.com>
2017-09-09 20:23:14 +0000

    Add all changes to try for touch input

--- dashcam.py
+++ dashcam.py
@@ -42,17 +42,41 @@ class Button:
 		self.rect = rect
 		self.icon = None
 		self.callback = None
+		self.value = None
 		for key, value in kwargs.iteritems():
 			if key == "icon": self.icon = value
 			elif key == "callback": self.callback = value
+			elif key == "value": self.value = value
 
-buttons = [
+	def selected(self, pos):
+		x1 = self.rect[0]
+		y1 = self.rect[1]
+		x2 = x1 + self.rect[2] - 1
+		y2 = y1 + self.rect[3] - 1
+		if ((pos[0] >= x1) and (pos[0] <= x2) and (pos[1] >= y1) and (pos[1] <= y2)):
+			if self.callback:		
+				if self.value is None:
+					self.callback()
+				else:
+					self.callback(self.value)
+			return True
+		return False
+
+	def draw(self, screen):
+		if self.icon:
+			screen.blit(self.icon.bitmap,(self.rect[0]+(self.rect[2]-self.icon.bitmap.get_width())/2, self.rect[1]+(self.rect[3]-self.icon.bitmap.get_height())/2))
+
+buttons = [Button((0,0,50,50), icon='go', callback='start_video')]
+
+def start_video():
+	screen.fill((0,0,0))
 
 def main():
 	for event in pygame.event.get():
-		if (event.type is MOUSEBUTTONDOWN):
+		if(event.type is pygame.MOUSEBUTTONDOWN):
 			pos = pygame.mouse.get_pos()
-			for b in buttons
+			for b in buttons:
+				if b.selected: break
 
         stream = io.BytesIO()
         camera.capture(stream, use_video_port=True, format='raw')
