85de2774390c4b063a258ed3490d271c6b1387f1
the-raspberry-pi-guy <theraspberrypiguy@gmail.com>
2017-09-09 19:05:39 +0000

    Neaten up dashcam program, add comments

--- dashcam.py
+++ dashcam.py
@@ -2,43 +2,38 @@
 # A Raspberry Pi powered, GPS enabled, 3D printed bicycle dashcam
 # By Matthew Timmons-Brown, The Raspberry Pi Guy
 
+# Import necessary modules
 import pygame
 import picamera
 import os
 import sys
 import io
 import atexit
+# Change path and import Adafruit's yuv2rgb library
 sys.path.insert(0, "/home/pi/bike_dashcam/libraries")
 import yuv2rgb
 
+# Change screen to PiTFT, init framebuffer/touchscreen environment variables
 os.putenv('SDL_VIDEODRIVER', 'fbcon')
 os.putenv('SDL_FBDEV'      , '/dev/fb1')
 os.putenv('SDL_MOUSEDRV'   , 'TSLIB')
 os.putenv('SDL_MOUSEDEV'   , '/dev/input/touchscreen')
 
+# Set size of display and create buffers for display data
 size = width, height = 320, 240
 rgb = bytearray(320 * 240 * 3)
 yuv = bytearray(320 * 240 * 3 / 2)
 
+# Startup and setup Pygame, load graphics
 pygame.init()
 pygame.mouse.set_visible(False)
 screen = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
-
-#screen = pygame.display.set_mode(size)
 go_button = pygame.image.load("/home/pi/bike_dashcam/media/go.bmp")
 
+# Startup and setup the Raspberry Pi official camera
 camera = picamera.PiCamera()
 atexit.register(camera.close)
-
-sizeData = [ # Camera parameters for different size settings
- # Full res      Viewfinder  Crop window
- [(2592, 1944), (320, 240), (0.0   , 0.0   , 1.0   , 1.0   )], # Large
- [(1920, 1080), (320, 180), (0.1296, 0.2222, 0.7408, 0.5556)], # Med
- [(1440, 1080), (320, 240), (0.2222, 0.2222, 0.5556, 0.5556)]] # Small
-
-sizeMode = 0
-
-camera.resolution = sizeData[sizeMode][1]
+camera.resolution = size
 camera.crop = (0.0,0.0,1.0,1.0)
 
 while True:
@@ -47,11 +42,7 @@ while True:
 	stream.seek(0)
 	stream.readinto(yuv)
 	stream.close()
-
-	yuv2rgb.convert(yuv, rgb, sizeData[sizeMode][1][0], sizeData[sizeMode][1][1])
-	img = pygame.image.frombuffer(rgb[0:(sizeData[sizeMode][1][0] * sizeData[sizeMode][1][1] * 3)], sizeData[sizeMode][1], 'RGB')
-
-#	yuv2rgb.convert(yuv, rgb, size[0], size[1])
-#	img = pygame.image.frombuffer(rgb[0:(size[0]*size[1]*3)], size, 'RGB')
-	screen.blit(img, ((320 - img.get_width() ) / 2, (240 - img.get_height()) / 2))
+	yuv2rgb.convert(yuv, rgb, size[0], size[1])
+	img = pygame.image.frombuffer(rgb[0:(size[0]*size[1]*3)], size, 'RGB')
+	screen.blit(img, ((width - img.get_width() ) / 2, (height - img.get_height()) / 2))
 	pygame.display.update()
