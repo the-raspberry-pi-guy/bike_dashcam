eae4b06bd798eab3a46bdd5b7452411bb7fb02e1
the-raspberry-pi-guy <theraspberrypiguy@gmail.com>
2017-09-09 14:01:00 +0000

    Update dascham with pygame GO button load

--- dashcam.py
+++ dashcam.py
@@ -5,12 +5,20 @@
 import pygame
 import picamera
 import os
+import sys
+import io
 
 os.putenv('SDL_VIDEODRIVER', 'fbcon')
 os.putenv('SDL_FBDEV'      , '/dev/fb1')
 os.putenv('SDL_MOUSEDRV'   , 'TSLIB')
 os.putenv('SDL_MOUSEDEV'   , '/dev/input/touchscreen')
 
+size = width, height = 320, 240
+
 pygame.init()
 pygame.mouse.set_visible(False)
-screen = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
+
+screen = pygame.display.set_mode(size)
+go_button = pygame.image.load("/home/pi/bike_dashcam/media/go.bmp")
+
+
